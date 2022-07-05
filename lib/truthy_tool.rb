c855d7905bac207f90d1c089fefc3991b0f7e3b9
davidteren <drteren@gmail.com>
2022-07-05 07:23:25 +0200

    add solution

--- lib/truthy_tool.rb
+++ lib/truthy_tool.rb
@@ -6,4 +6,15 @@ Zeitwerk::Loader.for_gem.setup
 
 # Main namespace.
 module TruthyTool
+  module_function
+
+  def is_truthy?(val)
+    !(val.eql?(false) ||
+      val.eql?(nil) ||
+      val.eql?(0) ||
+      val.eql?("") ||
+      val.eql?("\n") ||
+      val.eql?([]) ||
+      val.eql?({}))
+  end
 end
