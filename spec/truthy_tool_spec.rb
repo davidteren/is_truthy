48e001dab602ff3ae26d5c482b247dc4b3622b0a
davidteren <drteren@gmail.com>
2022-07-05 07:23:10 +0200

    add specs

---
+++ spec/truthy_tool_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+RSpec.describe TruthyTool do
+
+  describe ".is_truthy??" do
+    it { expect(described_class.is_truthy?(false)).to eq(false) } # false
+    it { expect(described_class.is_truthy?(nil)).to eq(false) } # false
+    it { expect(described_class.is_truthy?(0)).to eq(false) } # false
+    it { expect(described_class.is_truthy?("")).to eq(false) } # false
+    it { expect(described_class.is_truthy?("")).to eq(false) } # false
+    it { expect(described_class.is_truthy?("\n")).to eq(false) } # false
+    it { expect(described_class.is_truthy?("abc")).to eq(true)}  # true
+    it { expect(described_class.is_truthy?(42)).to be_truthy}  # true
+    it { expect(described_class.is_truthy?([])).to eq(false) } # false
+    it { expect(described_class.is_truthy?([1])).to be_truthy}  # true
+    it { expect(described_class.is_truthy?([1, 5])).to be_truthy}  # true
+    it { expect(described_class.is_truthy?({})).to eq(false) } # false
+    it { expect(described_class.is_truthy?({a: :b})).to be_truthy}  # true
+    end
+end
+
+
