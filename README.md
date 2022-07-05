01ec7e20499723c6b5cd7b54c27101d4203c00ee
davidteren <drteren@gmail.com>
2022-07-05 07:36:58 +0200

    update readme

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Based on this [tweet](https://twitter.com/ShinoKouda/status/1543921321163341828)
 
 
 > ![img.png](img.png)
+> 
+
+### Solution
+
+> ![img_1.png](img_1.png)
+> 
--- spec/truthy_tool_spec.rb
+++ spec/truthy_tool_spec.rb
@@ -10,12 +10,12 @@ RSpec.describe TruthyTool do
     it { expect(described_class.is_truthy?("")).to eq(false) } # false
     it { expect(described_class.is_truthy?("\n")).to eq(false) } # false
     it { expect(described_class.is_truthy?("abc")).to eq(true)}  # true
-    it { expect(described_class.is_truthy?(42)).to be_truthy}  # true
+    it { expect(described_class.is_truthy?(42)).to eq(true)}  # true
     it { expect(described_class.is_truthy?([])).to eq(false) } # false
-    it { expect(described_class.is_truthy?([1])).to be_truthy}  # true
-    it { expect(described_class.is_truthy?([1, 5])).to be_truthy}  # true
+    it { expect(described_class.is_truthy?([1])).to eq(true)}  # true
+    it { expect(described_class.is_truthy?([1, 5])).to eq(true)}  # true
     it { expect(described_class.is_truthy?({})).to eq(false) } # false
-    it { expect(described_class.is_truthy?({a: :b})).to be_truthy}  # true
+    it { expect(described_class.is_truthy?({a: :b})).to eq(true)}  # true
     end
 end
 
